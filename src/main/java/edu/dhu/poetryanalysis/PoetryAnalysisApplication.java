95b8e9cd35f17ad1385314e5e0ad8706e8a9bfac
hositsuki <yukisakuranoyume@gmail.com>
2022-04-11 16:54:53 +0900

    Initial commit

---
+++ src/main/java/edu/dhu/poetryanalysis/PoetryAnalysisApplication.java
@@ -0,0 +1,13 @@
+package edu.dhu.poetryanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetryAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoetryAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/dhu/poetryanalysis/PoetryAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dhu.poetryanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetryAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
